b8f226104a45aaca07dca34abfdbd30c5ef338ea
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2023-11-21 12:37:20 +0100

    fixed cors

--- backend/code/public/index.php
+++ backend/code/public/index.php
@@ -4,6 +4,7 @@ namespace oszimt\petweightwatcher;
 
 require_once dirname(__DIR__) .'/vendor/autoload.php';
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
 
 $factory = new Factory();
 $router = $factory->createRouter();
